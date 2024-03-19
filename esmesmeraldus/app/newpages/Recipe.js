4a7405b3c985113d4dab382c138bd4801d2aeaf0
ZeusEstrella <zeusaixcel31@gmail.com>
2024-03-18 20:21:22 -0600

    new

---
+++ esmesmeraldus/app/newpages/Recipe.js
@@ -0,0 +1,102 @@
+<div className="Recipes" style={{width: 1512, height: 1692, position: 'relative', background: '#E9E5D6'}}>
+  <div className="BottomOfPage" style={{width: 1512, height: 321.32, left: 0, top: 1370.66, position: 'absolute'}}>
+    <div className="Rectangle39" style={{width: 1512, height: 321.32, left: 0, top: 0, position: 'absolute', background: '#464E2E'}} />
+    <div className="Icons" style={{width: 1365.76, height: 135.24, left: 42.47, top: 63.23, position: 'absolute'}}>
+      <div className="Terms" style={{width: 208.98, height: 132.94, left: 1156.78, top: 0, position: 'absolute'}}>
+        <div className="PrivacyPolicy" style={{width: 208.98, height: 44.31, left: 0, top: 88.63, position: 'absolute', color: '#E9E5D6', fontSize: 24, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Privacy Policy</div>
+        <div className="TermsOfService" style={{width: 208.98, height: 44.31, left: 0, top: 0, position: 'absolute', color: '#E9E5D6', fontSize: 24, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Terms of Service</div>
+        <div className="ReturnPolicy" style={{width: 208.98, height: 44.31, left: 0, top: 44.31, position: 'absolute', color: '#E9E5D6', fontSize: 24, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Return Policy</div>
+      </div>
+      <div className="MaskGroup" style={{width: 142.45, height: 47.07, left: 967.34, top: 0, position: 'absolute'}}>
+        <div className="MaskGroup" style={{width: 24.56, height: 36.87, left: 117.89, top: 5.10, position: 'absolute'}}>
+          <img className="Instagram" style={{width: 24.56, height: 36.87, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/25x37" />
+          <div className="Rectangle45" style={{width: 22.69, height: 32.12, left: 1.87, top: 2.37, position: 'absolute', background: '#E9E5D6'}} />
+        </div>
+        <div className="MaskGroup" style={{width: 20.55, height: 15.90, left: 59.13, top: 15.59, position: 'absolute'}}>
+          <img className="Linkedin" style={{width: 20.55, height: 15.90, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/21x16" />
+          <div className="Rectangle45" style={{width: 21.28, height: 32.12, left: 1.80, top: -9.04, position: 'absolute', background: '#E9E5D6'}} />
+        </div>
+        <div className="MaskGroup" style={{width: 21.28, height: 21, left: 29.57, top: 11.70, position: 'absolute'}}>
+          <img className="Pinterest" style={{width: 21.28, height: 21, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/21x21" />
+          <div className="Rectangle45" style={{width: 21.28, height: 32.12, left: -0, top: -4.22, position: 'absolute', background: '#E9E5D6'}} />
+        </div>
+        <div className="MaskGroup" style={{width: 21.28, height: 47.07, left: 0, top: 0, position: 'absolute'}}>
+          <img className="Email" style={{width: 21.28, height: 47.07, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/21x47" />
+          <div className="Rectangle45" style={{width: 21.28, height: 32.12, left: 0, top: 6.55, position: 'absolute', background: '#E9E5D6'}} />
+        </div>
+        <img className="Facebook" style={{width: 21.28, height: 20.18, left: 88.33, top: 12.52, position: 'absolute'}} src="https://via.placeholder.com/21x20" />
+        <div className="Rectangle45" style={{width: 148.32, height: 62.04, left: -5.88, top: -5.92, position: 'absolute', background: '#E9E5D6'}} />
+      </div>
+      <div className="Rectangle46" style={{width: 445.59, height: 44.31, left: 0, top: 88.63, position: 'absolute', background: '#E9E5D6', borderRadius: 10}} />
+      <div className="Subscribe" style={{width: 32.12, height: 48.88, left: 435.20, top: 135.24, position: 'absolute', transform: 'rotate(-179.93deg)', transformOrigin: '0 0'}}>
+        <img className="BackArrow" style={{width: 32.12, height: 48.88, left: 0, top: 0, position: 'absolute', transform: 'rotate(-179.93deg)', transformOrigin: '0 0'}} src="https://via.placeholder.com/32x49" />
+        <div className="Rectangle47" style={{width: 136.18, height: 134.17, left: 17.76, top: 23.02, position: 'absolute', transform: 'rotate(-179.93deg)', transformOrigin: '0 0', background: '#362706'}} />
+      </div>
+    </div>
+    <div className="SubscribeForMoreNewsWeSendOutNewsDealsAndRecipesDonTMissOut" style={{width: 530.19, left: 42.47, top: 35.32, position: 'absolute', color: '#E9E5D6', fontSize: 24, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}><br/>Subscribe for more news!<br/>We send out news, deals and recipes! Don't miss out!</div>
+    <div className="EnterEmailAddress" style={{width: 209.26, left: 53.21, top: 159.79, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Enter Email Address</div>
+  </div>
+  <div className="Slogan" style={{width: 1512, height: 206.38, left: 0, top: 1164.28, position: 'absolute'}}>
+    <div className="Rectangle40" style={{width: 1512, height: 206.38, left: 0, top: 0, position: 'absolute', background: '#34713A'}} />
+    <div className="CoffeeRichAsAnEmerald" style={{width: 450.04, height: 76.83, left: 560.36, top: 71.19, position: 'absolute', textAlign: 'center', color: '#E9E5D6', fontSize: 32, fontFamily: 'Baskervville', fontWeight: '400', wordWrap: 'break-word'}}>Coffee, rich as an emerald.</div>
+  </div>
+  <div className="Recipes" style={{width: 397.20, height: 72.51, left: 686.39, top: 266.57, position: 'absolute', color: '#362706', fontSize: 48, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Recipes</div>
+  <div className="Group17" style={{width: 373.91, height: 438.54, left: 81.17, top: 461.92, position: 'absolute'}}>
+    <div className="Rectangle41" style={{width: 373.91, height: 438.54, left: 0, top: 0, position: 'absolute', background: '#464E2E', borderRadius: 30}} />
+    <div className="CoffeeCookiesRecipeDeliciouslyAddictive" style={{width: 316.42, height: 97.31, left: 28.75, top: 287.15, position: 'absolute', textAlign: 'center', color: '#E9E5D6', fontSize: 26, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Coffee Cookies Recipe: Deliciously Addictive</div>
+    <img className="Image16" style={{width: 373.91, height: 243.65, left: 0, top: 0, position: 'absolute', borderTopLeftRadius: 20, borderTopRightRadius: 20}} src="https://via.placeholder.com/374x244" />
+  </div>
+  <div className="Group18" style={{width: 376.63, height: 438.54, left: 567.48, top: 461.92, position: 'absolute'}}>
+    <div className="Rectangle41" style={{width: 373.91, height: 438.54, left: 2.72, top: 0, position: 'absolute', background: '#464E2E', borderRadius: 30}} />
+    <div className="EsmeraldusCappuccinoRecipe" style={{width: 316.42, height: 97.31, left: 35.36, top: 287.15, position: 'absolute', textAlign: 'center', color: '#E9E5D6', fontSize: 26, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Esmeraldus Cappuccino Recipe<br/><br/></div>
+    <img className="Image17" style={{width: 376.63, height: 243.65, left: -0, top: 0, position: 'absolute', borderTopLeftRadius: 20, borderTopRightRadius: 20}} src="https://via.placeholder.com/377x244" />
+  </div>
+  <div className="Group19" style={{width: 376.63, height: 438.54, left: 1056.50, top: 463.64, position: 'absolute'}}>
+    <div className="Rectangle41" style={{width: 373.91, height: 438.54, left: 0, top: 0, position: 'absolute', background: '#464E2E', borderRadius: 30}} />
+    <div className="EsmeraldusLatteRecipe" style={{width: 316.42, height: 97.31, left: 28.75, top: 293.78, position: 'absolute', textAlign: 'center', color: '#E9E5D6', fontSize: 26, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Esmeraldus Latte Recipe</div>
+    <img className="Image18" style={{width: 376.63, height: 243.65, left: 0, top: 0, position: 'absolute', borderTopLeftRadius: 20, borderTopRightRadius: 20}} src="https://via.placeholder.com/377x244" />
+  </div>
+  <div className="HomeLearnRecipes" style={{width: 887.22, height: 59.70, left: 68.65, top: 287.92, position: 'absolute', color: '#3D8244', fontSize: 28, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Home / Learn / Recipes<br/></div>
+  <div className="TopBar" style={{width: 1540.36, height: 235.84, left: -10.98, top: -7.86, position: 'absolute'}}>
+    <div className="Rectangle39" style={{width: 1540.36, height: 235.84, left: 0, top: 0, position: 'absolute', background: '#464E2E'}} />
+    <img className="EsmeraldusColombianCoffeeLogo2" style={{width: 154.76, height: 154.76, left: 49.45, top: 54.64, position: 'absolute'}} src="https://via.placeholder.com/155x155" />
+    <div className="Pages" style={{width: 1087.03, height: 41.37, left: 412.56, top: 169.35, position: 'absolute'}}>
+      <div className="IconShoppingCart" style={{width: 40.45, height: 37.15, left: 1046.57, top: 1.90, position: 'absolute'}}>
+        <div className="Vector" style={{width: 5.06, height: 4.64, left: 10.11, top: 30.18, position: 'absolute', background: '#E9E5D6'}}></div>
+        <div className="Vector" style={{width: 5.06, height: 4.64, left: 27.81, top: 30.18, position: 'absolute', background: '#E9E5D6'}}></div>
+        <div className="Vector" style={{width: 36.69, height: 25.54, left: 0, top: 2.32, position: 'absolute', background: '#E9E5D6'}}></div>
+        <div className="Vector" style={{width: 40.45, height: 37.15, left: 0, top: 0, position: 'absolute'}}></div>
+      </div>
+      <div className="IconSearch" style={{width: 40.67, height: 37.15, left: 983.26, top: 4.22, position: 'absolute'}}>
+        <div className="Vector" style={{width: 34.36, height: 31.38, left: 2.50, top: 2.28, position: 'absolute', background: '#E9E5D6'}}></div>
+        <div className="Vector" style={{width: 40.67, height: 37.15, left: 0, top: 0, position: 'absolute'}}></div>
+      </div>
+      <div className="Home" style={{width: 107, height: 39.16, left: 0, top: 0.89, position: 'absolute', color: '#E9E5D6', fontSize: 28, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Home</div>
+      <div className="Shop" style={{width: 83, height: 26.11, left: 173.04, top: 0.89, position: 'absolute', color: '#E9E5D6', fontSize: 28, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Shop</div>
+      <div className="About" style={{width: 178, height: 26.83, left: 508.91, top: 1.90, position: 'absolute', color: '#E9E5D6', fontSize: 28, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>About</div>
+      <div className="Learn" style={{width: 154.41, height: 30.46, left: 335.45, top: 0, position: 'absolute', color: '#E9E5D6', fontSize: 28, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Learn</div>
+      <div className="MaskGroup" style={{width: 57.54, height: 26.11, left: 399.96, top: 7.42, position: 'absolute'}}>
+        <img className="ExpandArrow" style={{width: 57.54, height: 26.11, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/58x26" />
+        <div className="Rectangle58" style={{width: 42.46, height: 40.37, left: 15.73, top: -8.52, position: 'absolute', background: '#E9E5D6'}} />
+      </div>
+      <div className="MaskGroup" style={{width: 57.54, height: 26.11, left: 578.85, top: 7.42, position: 'absolute'}}>
+        <img className="ExpandArrow" style={{width: 57.54, height: 26.11, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/58x26" />
+        <div className="Rectangle58" style={{width: 42.46, height: 40.37, left: 15.73, top: -8.52, position: 'absolute', background: '#E9E5D6'}} />
+      </div>
+      <div className="MaskGroup" style={{width: 57.54, height: 26.11, left: 227.27, top: 7.42, position: 'absolute'}}>
+        <img className="ExpandArrow" style={{width: 57.54, height: 26.11, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/58x26" />
+        <div className="Rectangle58" style={{width: 42.46, height: 40.37, left: 15.73, top: -8.52, position: 'absolute', background: '#E9E5D6'}} />
+      </div>
+    </div>
+    <div className="SignUpLoginButton" style={{width: 371.93, height: 51.83, left: 1131.28, top: 54.64, position: 'absolute'}}>
+      <div className="Rectangle7" style={{width: 167.76, height: 49.36, left: 2.72, top: 0, position: 'absolute', background: '#3D8244', borderRadius: 10}} />
+      <div className="Rectangle7" style={{width: 167.76, height: 49.36, left: 200.54, top: 0, position: 'absolute', background: '#362706', borderRadius: 10}} />
+      <div className="Login" style={{width: 174.11, height: 43.60, left: 197.82, top: 8.23, position: 'absolute', textAlign: 'center', color: '#E9E5D6', fontSize: 24, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Login</div>
+      <div className="SignUp" style={{width: 174.11, height: 43.60, left: 0, top: 8.23, position: 'absolute', textAlign: 'center', color: '#E9E5D6', fontSize: 24, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Sign Up</div>
+    </div>
+    <div className="Promo" style={{width: 1512, height: 31.59, left: 10.98, top: 7.86, position: 'absolute'}}>
+      <div className="Rectangle17" style={{width: 1512, height: 27.03, left: 0, top: 0, position: 'absolute', background: '#34713A'}} />
+      <div className="FreeDeliveryForPurchasesOf50OrOverOnlyInCalgary" style={{width: 910.06, height: 31.59, left: 436.41, top: 0, position: 'absolute', color: '#E9E5D6', fontSize: 24, fontFamily: 'Average', fontWeight: '400', wordWrap: 'break-word'}}>Free delivery for purchases of $50 or over! Only in Calgary! <br/></div>
+    </div>
+  </div>
+</div>

9c90a6da2d311af8ce311ecab206dbbd62b4ea4e
japsimran04 <japsimrankaur.Nanda@edu.sait.ca>
2024-03-19 03:21:38 -0600

    products page done so far

--- esmesmeraldus/app/page.js
+++ esmesmeraldus/app/page.js
@@ -6,12 +6,9 @@ export default function Page() {
       <h1 className="text-2xl font-extrabold dark:text-white">ESMERALDUS COLOMBIAN COFFEE</h1>
 
       <h2 className="text-2xl font-extrabold dark:text-white">Lets get started </h2>
-      <Link href="/profile-page" className="hover:underline">profile-page</Link>
-      <Link href="/category" className="hover:underline"></Link>
-      <Link href="/newpages" className="hover:underline">category</Link>
+      <Link href="/ColumbianCoffee" className="hover:underline">ColumbianCoffee</Link>
+      <Link href="/newpages" className="hover:underline">newpages</Link>
 
-
-      <br />
     </>
   );
 }

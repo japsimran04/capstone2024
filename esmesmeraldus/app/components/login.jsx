04ad3aa3fd8b67cc2000dd85209ba81f26765b48
raquel-castoldi <raquelcastoldi16@gmail.com>
2024-03-22 15:35:57 -0600

    firebase config added

--- esmesmeraldus/app/components/login.jsx
+++ esmesmeraldus/app/components/login.jsx
@@ -1,55 +1,14 @@
-import { auth, googleProvider } from "../config/firebase";
-import {
-  createUserWithEmailAndPassword,
-  signInWithPopup,
-  signOut,
-} from "firebase/auth";
-import { useState } from "react";
+import React from "react";
 
-export const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <p>
+                Login form here
+            </p>
+        </div>
+    );
+}
 
-  const signIn = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const signInWithGoogle = async () => {
-    try {
-      await signInWithPopup(auth, googleProvider);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const logout = async () => {
-    try {
-      await signOut(auth);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  return (
-    <div>
-      <input
-        placeholder="Email..."
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        placeholder="Password..."
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={signIn}> Sign In</button>
-
-      <button onClick={signInWithGoogle}> Sign In With Google</button>
-
-      <button onClick={logout}> Logout </button>
-    </div>
-  );
-};
+export default Login;

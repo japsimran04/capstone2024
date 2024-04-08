a0acbb44cd4f16bc6119b00a4bae0e226c7e6626
japsimran04 <japsimrankaur.Nanda@edu.sait.ca>
2024-04-08 01:14:24 -0600

    added css

---
+++ esmesmeraldus/app/components/aboutus.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our website! We are a dedicated team of professionals passionate about...</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed viverra felis non tincidunt. In fringilla, eros eu lacinia suscipit, libero mauris tempor dui, ut pharetra ligula eros a nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut sed dolor vel quam iaculis condimentum. Cras ultricies diam at libero finibus, quis facilisis nisi ultricies. Integer accumsan purus vel felis malesuada, sit amet suscipit urna gravida.</p>
+      <p>Phasellus ultrices dolor id congue consectetur. Morbi nec quam a risus convallis faucibus id ut ex. Suspendisse at velit nunc. Mauris accumsan, mauris in hendrerit ultricies, justo ligula sodales odio, a ullamcorper quam dui at libero. Sed vehicula metus eget quam placerat, vitae consequat lectus efficitur. Vestibulum pretium neque at semper suscipit. Ut auctor felis at dolor pharetra faucibus. Fusce quis leo vel dui convallis consectetur id et mi. Sed gravida tristique nisi vel lacinia. Vestibulum aliquam nibh nec ipsum posuere, vel sodales dolor fermentum. Cras et tristique purus. Suspendisse sit amet ligula et purus tristique facilisis vel at tortor. Nulla facilisi. Fusce vestibulum nisl at massa hendrerit, eu tincidunt sem lacinia.</p>
+    </div>
+  );
+}
+
+export default AboutUs;

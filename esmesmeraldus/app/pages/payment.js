fb1c430d03bab143eb729be3c62c7d041aa5ba0a
raquel-castoldi <raquelcastoldi16@gmail.com>
2024-04-16 23:50:56 -0600

    reinstalled mui

--- esmesmeraldus/app/pages/payment.js
+++ esmesmeraldus/app/pages/payment.js
@@ -1,9 +1,30 @@
-import { Input } from '@mui/material';
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
-// import MaskedInput from 'react-input-mask';
+import { styled } from '@mui/system';
+import { Container, Typography, TextField, Button, Grid, Divider, MenuItem } from '@mui/material';
+import MaskedInput from 'react-input-mask'; // Import input masking library
+
+
+const useStyles = styled((theme) => ({
+  root: {
+    marginTop: theme.spacing(4),
+  },
+  title: {
+    marginBottom: theme.spacing(2),
+  },
+  form: {
+    marginBottom: theme.spacing(2),
+  },
+  button: {
+    marginTop: theme.spacing(2),
+  },
+  divider: {
+    margin: theme.spacing(2, 0),
+  },
+}));
 
 const Payment = () => {
+  const classes = useStyles();
   const [cardNumber, setCardNumber] = useState('');
   const [cvv, setCvv] = useState('');
   const [expiryDate, setExpiryDate] = useState('');
@@ -32,6 +53,7 @@ const Payment = () => {
     QC: 0.14975,
     SK: 0.11,
     YT: 0.05,
+
   };
   
   const totalWithTaxes = totalAmount * (1 + provinceTaxes[province]);
@@ -56,363 +78,160 @@ const Payment = () => {
   };
 
   return (
-    <div style={{ marginTop: '20px', fontFamily: 'Arial, sans-serif' }}>
-      <form onSubmit={processPayment}>
-        <h2 style={{ fontFamily: 'Arial, sans-serif' }}>Checkout</h2>
-        <div style={{ marginBottom: '10px' }}>
-          <input
-            type="text"
-            placeholder="First Name"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
-          <input
-            type="text"
-            placeholder="Last Name"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-        </div>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Street Address"
-          value={street}
-          onChange={(e) => setStreet(e.target.value)}
-          required
-        />
-        <select
-          value={province}
-          onChange={(e) => setProvince(e.target.value)}
-          required
-        >
-          {Object.keys(provinceTaxes).map((province) => (
-            <option key={province} value={province}>
-              {province}
-            </option>
-          ))}
-        </select>
-        <input
-          type="text"
-          placeholder="Country"
-          value={country}
-          disabled
-        />
-        <h2 style={{ fontFamily: 'Arial, sans-serif' }}>Payment Method</h2>
-        <Input
-          mask="9999 9999 9999 9999"
-          value={cardNumber}
-          onChange={(e) => setCardNumber(e.target.value)}
-        >
-          {(inputProps) => (
-            <input
-              type="text"
-              placeholder="Card Number"
-              {...inputProps}
+    <Container className={classes.root}>
+      <form className={classes.form} onSubmit={processPayment}>
+        <Typography variant="h4" style={{fontFamily:'Playfair Display, serif'}} className={classes.title}>
+          Checkout
+        </Typography>
+        <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              label="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
               required
+              style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif'}}
             />
-          )}
-        </Input>
-        <Input
-          mask="99/99"
-          value={expiryDate}
-          onChange={(e) => setExpiryDate(e.target.value)}
-        >
-          {() => (
-            <input
-              type="text"
-              placeholder="Expiration Date"
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              label="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
               required
+              style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif'}}
             />
-          )}
-        </Input>
-        <input
-          type="text"
-          placeholder="CVV"
-          value={cvv}
-          onChange={(e) => setCvv(e.target.value)}
-          maxLength={3}
-          required
-        />
-        <div>
-          <button
-            type="submit"
-            style={{ backgroundColor: '#34713A', color: 'white' }}
-          >
-            Place Order
-          </button>
-        </div>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              label="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif'}}
+
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              label="Street Address"
+              value={street}
+              onChange={(e) => setStreet(e.target.value)}
+              required
+              style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif'}}
+            />
+          </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              select
+              variant="outlined"
+              fullWidth
+              label="Province"
+              value={province}
+              onChange={(e) => setProvince(e.target.value)}
+              required
+              style={{ marginBottom: '10px' }}>
+              {Object.keys(provinceTaxes).map((province) => (
+                <MenuItem key={province} value={province}>
+                  {province}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              label="Country"
+              value={country}
+              disabled
+              style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif'}}
+
+            />
+          </Grid>
+        </Grid>
+        <Typography variant="h4" style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif' }}>
+              Payment Method
+          </Typography>
+          <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <MaskedInput
+              mask="9999 9999 9999 9999" // Set card number mask
+              value={cardNumber}
+              onChange={(e) => setCardNumber(e.target.value)}
+            >
+              {(inputProps) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  label="Card Number"
+                  {...inputProps}
+                  required
+                />
+              )}
+            </MaskedInput>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <MaskedInput
+              mask="99/99"
+              value={expiryDate}
+              onChange={(e) => setExpiryDate(e.target.value)}
+            >
+              {() => <TextField
+                variant="outlined"
+                fullWidth
+                label="Expiration Date"
+                required
+              />}
+            </MaskedInput>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              label="CVV"
+              value={cvv}
+              onChange={(e) => setCvv(e.target.value)}
+              inputProps={{ maxLength: 3 }} // Set max length to 3 for CVV
+              required
+              style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif'}}
+            />
+          </Grid>
+        </Grid>
+        <Typography variant="h4" style={{fontFamily:'Playfair Display, serif'}}>Order Summary</Typography>
+      <Typography style={{fontFamily:'Playfair Display, serif'}}>Subtotal: ${totalAmount}</Typography>
+      <Typography style={{fontFamily:'Playfair Display, serif'}}>Taxes: ${(totalWithTaxes - totalAmount).toFixed(2)}</Typography>
+      <Typography style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif' }}>Total Amount (including taxes): ${totalWithTaxes.toFixed(2)}</Typography>
+        <Button
+          variant="contained"
+          style={{ backgroundColor: '#34713A', color: 'white', fontFamily:'Playfair Display, serif'}}
+          fullWidth
+          className={classes.button}
+          type="submit"
+        >
+          Place Order
+        </Button>
       </form>
-      <div>
-        <h2 style={{ fontFamily: 'Arial, sans-serif' }}>Order Summary</h2>
-        <p>Subtotal: ${totalAmount}</p>
-        <p>Taxes: ${(totalWithTaxes - totalAmount).toFixed(2)}</p>
-        <p>Total Amount (including taxes): ${totalWithTaxes.toFixed(2)}</p>
-      </div>
-      <div>
-        {transactions.map((transaction, index) => (
-          <div key={index}>
-            <p>Amount: {transaction.amount}</p>
-            <p>Status: {transaction.status}</p>
-            <p>Transaction Date: {transaction.transactionDate.toString()}</p>
-          </div>
-        ))}
-      </div>
-    </div>
+      <Divider className={classes.divider} />
+      {transactions.map((transaction, index) => (
+        <div key={index}>
+          <p>Amount: {transaction.amount}</p>
+          <p>Status: {transaction.status}</p>
+          <p>Transaction Date: {transaction.transactionDate.toString()}</p>
+        </div>
+      ))}
+    </Container>
   );
 };
 
 export default Payment;
-// import React, { useState } from 'react';
-// import { useLocation } from 'react-router-dom';
-// // import { makeStyles } from '@material-ui/core/styles';
-// // import {
-// //   Container,
-// //   Typography,
-// //   TextField,
-// //   Button,
-// //   Grid,
-// //   Divider,
-// //   MenuItem,
-// // } from '@material-ui/core';
-// import MaskedInput from 'react-input-mask'; // Import input masking library
-
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     marginTop: theme.spacing(4),
-//   },
-//   title: {
-//     marginBottom: theme.spacing(2),
-//   },
-//   form: {
-//     marginBottom: theme.spacing(2),
-//   },
-//   button: {
-//     marginTop: theme.spacing(2),
-//   },
-//   divider: {
-//     margin: theme.spacing(2, 0),
-//   },
-// }));
-
-// const Payment = () => {
-//   const classes = useStyles();
-//   const [cardNumber, setCardNumber] = useState('');
-//   const [cvv, setCvv] = useState('');
-//   const [expiryDate, setExpiryDate] = useState('');
-//   const [street, setStreet] = useState('');
-//   const [province, setProvince] = useState('AB');
-//   const [country, setCountry] = useState('Canada');
-//   const [transactions, setTransactions] = useState([]);
-//   const [firstName, setFirstName] = useState('');
-//   const [lastName, setLastName] = useState('');
-//   const [email, setEmail] = useState('');
-
-//   const location = useLocation();
-//   const totalAmount = location.state ? location.state.totalAmount : 0;
-
-//   const provinceTaxes = {
-//     AB: 0.05,
-//     BC: 0.12,
-//     MB: 0.12,
-//     NB: 0.15,
-//     NL: 0.15,
-//     NT: 0.05,
-//     NS: 0.15,
-//     NU: 0.05,
-//     ON: 0.13,
-//     PE: 0.15,
-//     QC: 0.14975,
-//     SK: 0.11,
-//     YT: 0.05,
-
-//   };
-  
-//   const totalWithTaxes = totalAmount * (1 + provinceTaxes[province]);
-
-//   const processPayment = (event) => {
-//     event.preventDefault();
-    
-//     const transaction = {
-//       cardNumber,
-//       cvv,
-//       expiryDate,
-//       amount: totalAmount,
-//       status: 'Successful',
-//       transactionDate: new Date(),
-//     };
-
-//     setTransactions([...transactions, transaction]);
-
-//     setCardNumber('');
-//     setCvv('');
-//     setExpiryDate('');
-//   };
-
-//   return (
-//     <Container className={classes.root}>
-//       <form className={classes.form} onSubmit={processPayment}>
-//         <Typography variant="h4" style={{fontFamily:'Playfair Display, serif'}} className={classes.title}>
-//           Checkout
-//         </Typography>
-//         <Grid container spacing={2}>
-//         <Grid item xs={12} sm={6}>
-//         <Grid item xs={12} sm={6}>
-//             <TextField
-//               variant="outlined"
-//               fullWidth
-//               label="First Name"
-//               value={firstName}
-//               onChange={(e) => setFirstName(e.target.value)}
-//               required
-//               style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif'}}
-//             />
-//           </Grid>
-//           <Grid item xs={12} sm={6}>
-//             <TextField
-//               variant="outlined"
-//               fullWidth
-//               label="Last Name"
-//               value={lastName}
-//               onChange={(e) => setLastName(e.target.value)}
-//               required
-//               style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif'}}
-//             />
-//           </Grid>
-//           <Grid item xs={12}>
-//             <TextField
-//               variant="outlined"
-//               fullWidth
-//               label="Email"
-//               value={email}
-//               onChange={(e) => setEmail(e.target.value)}
-//               required
-//               style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif'}}
-
-//             />
-//           </Grid>
-//           <Grid item xs={12}>
-//             <TextField
-//               variant="outlined"
-//               fullWidth
-//               label="Street Address"
-//               value={street}
-//               onChange={(e) => setStreet(e.target.value)}
-//               required
-//               style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif'}}
-//             />
-//           </Grid>
-//           </Grid>
-//           <Grid item xs={12}>
-//             <TextField
-//               select
-//               variant="outlined"
-//               fullWidth
-//               label="Province"
-//               value={province}
-//               onChange={(e) => setProvince(e.target.value)}
-//               required
-//               style={{ marginBottom: '10px' }}>
-//               {Object.keys(provinceTaxes).map((province) => (
-//                 <MenuItem key={province} value={province}>
-//                   {province}
-//                 </MenuItem>
-//               ))}
-//             </TextField>
-//           </Grid>
-//           <Grid item xs={12}>
-//             <TextField
-//               variant="outlined"
-//               fullWidth
-//               label="Country"
-//               value={country}
-//               disabled
-//               style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif'}}
-
-//             />
-//           </Grid>
-//         </Grid>
-//         <Typography variant="h4" style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif' }}>
-//               Payment Method
-//           </Typography>
-//           <Grid container spacing={2}>
-//           <Grid item xs={12}>
-//             <MaskedInput
-//               mask="9999 9999 9999 9999" // Set card number mask
-//               value={cardNumber}
-//               onChange={(e) => setCardNumber(e.target.value)}
-//             >
-//               {(inputProps) => (
-//                 <TextField
-//                   variant="outlined"
-//                   fullWidth
-//                   label="Card Number"
-//                   {...inputProps}
-//                   required
-//                 />
-//               )}
-//             </MaskedInput>
-//           </Grid>
-//           <Grid item xs={12} sm={6}>
-//             <MaskedInput
-//               mask="99/99"
-//               value={expiryDate}
-//               onChange={(e) => setExpiryDate(e.target.value)}
-//             >
-//               {() => <TextField
-//                 variant="outlined"
-//                 fullWidth
-//                 label="Expiration Date"
-//                 required
-//               />}
-//             </MaskedInput>
-//           </Grid>
-//           <Grid item xs={12} sm={6}>
-//             <TextField
-//               variant="outlined"
-//               fullWidth
-//               label="CVV"
-//               value={cvv}
-//               onChange={(e) => setCvv(e.target.value)}
-//               inputProps={{ maxLength: 3 }} // Set max length to 3 for CVV
-//               required
-//               style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif'}}
-//             />
-//           </Grid>
-//         </Grid>
-//         <Typography variant="h4" style={{fontFamily:'Playfair Display, serif'}}>Order Summary</Typography>
-//       <Typography style={{fontFamily:'Playfair Display, serif'}}>Subtotal: ${totalAmount}</Typography>
-//       <Typography style={{fontFamily:'Playfair Display, serif'}}>Taxes: ${(totalWithTaxes - totalAmount).toFixed(2)}</Typography>
-//       <Typography style={{ marginBottom: '10px', fontFamily:'Playfair Display, serif' }}>Total Amount (including taxes): ${totalWithTaxes.toFixed(2)}</Typography>
-//         <Button
-//           variant="contained"
-//           style={{ backgroundColor: '#34713A', color: 'white', fontFamily:'Playfair Display, serif'}}
-//           fullWidth
-//           className={classes.button}
-//           type="submit"
-//         >
-//           Place Order
-//         </Button>
-//       </form>
-//       <Divider className={classes.divider} />
-//       {transactions.map((transaction, index) => (
-//         <div key={index}>
-//           <p>Amount: {transaction.amount}</p>
-//           <p>Status: {transaction.status}</p>
-//           <p>Transaction Date: {transaction.transactionDate.toString()}</p>
-//         </div>
-//       ))}
-//     </Container>
-//   );
-// };
-
-// export default Payment;
